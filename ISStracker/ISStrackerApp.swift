c7225b4fdbb2d0bfb26e7e6de1be31fbc3f06d00
NodMod1988 <niewerth.ruben@gmail.com>
2023-02-20 10:01:57 +0100

    Initial Commit

---
+++ ISStracker/ISStrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISStrackerApp.swift
+//  ISStracker
+//
+//  Created by Ruben Niewerth on 20.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ISStrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
