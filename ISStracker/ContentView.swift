6476908b3709b925f8a0c6596ed5c643d292ad2e
NodMod1988 <niewerth.ruben@gmail.com>
2023-02-20 10:11:29 +0100

    Initial Commit Develop

--- ISStracker/ContentView.swift
+++ ISStracker/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world 2!")
+            Text("Hello, world 3!")
+            Text("Hello, world 4!")
         }
         .padding()
     }
