d716b6f0312b4710f2b643e15ef3ba09ffc4c8de
NodMod1988 <niewerth.ruben@gmail.com>
2023-03-27 11:45:32 +0200

    set timer function in update location

---
+++ ISStracker/Service/Coordinator.swift
@@ -0,0 +1,66 @@
+//
+//  Coordinator.swift
+//  ISStracker
+//
+//  Created by Ruben Niewerth on 27.03.23.
+//
+
+import MapKit
+
+class Coordinator: NSObject, MKMapViewDelegate {
+    var parent: LocationMapView
+    
+    init(_ parent: LocationMapView) {
+        self.parent = parent
+    }
+    
+    // Update region when map is moved or zoomed
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        parent.region = mapView.region
+        print("New region: \(parent.region)")
+    }
+    
+    @objc func zoomIn() {
+        var region = parent.region
+        region.span.latitudeDelta *= 0.5
+        region.span.longitudeDelta *= 0.5
+        parent.region = region
+    }
+    
+    @objc func zoomOut() {
+        var region = parent.region
+        region.span.latitudeDelta *= 2
+        region.span.longitudeDelta *= 2
+        parent.region = region
+    }
+    
+    func zoomMap(by factor: Double) {
+        var region = parent.region
+        region.span.latitudeDelta *= factor
+        region.span.longitudeDelta *= factor
+        parent.region = region
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        let zoomInButton = UIButton(type: .custom)
+        zoomInButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        zoomInButton.addTarget(self, action: #selector(zoomIn), for: .touchUpInside)
+        
+        let zoomOutButton = UIButton(type: .custom)
+        zoomOutButton.setImage(UIImage(systemName: "minus"), for: .normal)
+        zoomOutButton.addTarget(self, action: #selector(zoomOut), for: .touchUpInside)
+        
+        mapView.addSubview(zoomInButton)
+        mapView.addSubview(zoomOutButton)
+        
+        zoomInButton.translatesAutoresizingMaskIntoConstraints = false
+        zoomInButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -16).isActive = true
+        zoomInButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -16).isActive = true
+        
+        zoomOutButton.translatesAutoresizingMaskIntoConstraints = false
+        zoomOutButton.bottomAnchor.constraint(equalTo: zoomInButton.topAnchor, constant: -16).isActive = true
+        zoomOutButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -16).isActive = true
+        
+    }
+    
+}
--- ISStracker/View/LocationMapView.swift
+++ ISStracker/View/LocationMapView.swift
@@ -11,15 +11,60 @@ import MapKit
 struct LocationMapView: UIViewRepresentable {
     
     @EnvironmentObject var viewModel:ISSLocationViewModel
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5))
+    @State private var zoomFactor = 1.0
     
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
-        mapView.showsUserLocation = false // Disable showing user location
+        mapView.showsUserLocation = false
+        mapView.delegate = Coordinator(self)         
+        let zoomInButton = UIButton(type: .custom)
+        zoomInButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        zoomInButton.addTarget(context.coordinator, action: #selector(Coordinator.zoomIn), for: .touchUpInside)
+        
+        let zoomOutButton = UIButton(type: .custom)
+        zoomOutButton.setImage(UIImage(systemName: "minus"), for: .normal)
+        zoomOutButton.addTarget(context.coordinator, action: #selector(Coordinator.zoomOut), for: .touchUpInside)
+        
+        mapView.addSubview(zoomInButton)
+        mapView.addSubview(zoomOutButton)
+        
+        zoomInButton.translatesAutoresizingMaskIntoConstraints = false
+        zoomInButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -16).isActive = true
+        zoomInButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -16).isActive = true
+        
+        zoomOutButton.translatesAutoresizingMaskIntoConstraints = false
+        zoomOutButton.bottomAnchor.constraint(equalTo: zoomInButton.topAnchor, constant: -16).isActive = true
+        zoomOutButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -16).isActive = true
+        
+ 
+        func zoomIn() {
+            zoomMap(by: 0.5)
+        }
+        
+
+        func zoomOut() {
+            zoomMap(by: 2.0)
+        }
+        
+     
+        func zoomMap(by factor: Double) {
+            var region = self.region
+            region.span.latitudeDelta *= factor
+            region.span.longitudeDelta *= factor
+            self.region = region
+        }
+        
         return mapView
     }
     
     func updateUIView(_ mapView: MKMapView, context: Context) {
-
+        
+        
+        Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { timer in
+            viewModel.fetchISSLocation()
+        }
+        
         if let issLocation = viewModel.issLocation {
             let span = MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
             let region = MKCoordinateRegion(center: issLocation, span: span)
@@ -39,5 +84,9 @@ struct LocationMapView: UIViewRepresentable {
             UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true)
         }
     }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
 }
-
